10416c37510561845d15fd8cd6e7ab81cd82ed23
alex <alex@localhost.localdomain>
2018-11-18 00:42:12 +0300

    deleted not use code

--- src/main/java/ru/hc128/Main.java
+++ src/main/java/ru/hc128/Main.java
@@ -5,14 +5,7 @@ import ru.hc128.util.IO.Input;
 import ru.hc128.util.IO.Output;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 
 public class Main {
@@ -33,24 +26,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        //Path path = Paths.get("/run/media/alex/ktom/tmp/test.xls");
-        //byte[] zipFileBytes= Files.readAllBytes(path);
-
-        /**
-        try(FileInputStream fin=new FileInputStream("/run/media/alex/ktom/tmp/test.xls");
-            FileOutputStream fos=new FileOutputStream("/run/media/alex/ktom/tmp/test1.xls"))
-        {
-            byte[] buffer = new byte[fin.available()];
-            // считываем буфер
-            fin.read(buffer, 0, buffer.length);
-            // записываем из буфера в файл
-            fos.write(buffer, 0, buffer.length);
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
-         */
 
         System.out.println("Start program");
         String iv_srt = "@#$$54214AEFDCAE";
@@ -89,58 +64,7 @@ public class Main {
                 outputed33[i] = encrypt(hc_enc, inputed[i]);
             }
             new Output().rewriteDecryptFileZip(pathToFile, outputed33);
-            /**
-            int i =0;
-            byte[][] ed= new byte[count(pathToFile)][];
-            byte[][] ed33  = new byte[count(pathToFile)][];
-            try(FileInputStream fin=new FileInputStream("/run/media/alex/ktom/tmp/test.xls"))
-            {
-                ed [i]  = new byte[fin.available()];
-                // считываем буфер
-                fin.read(ed[i] , 0, ed[i].length);
-
-                HC128Engine hc_enc = new HC128Engine(iv_srt.getBytes(), key_srt.getBytes());
-                hc_enc.reset();
-                ed33[i] = encrypt(hc_enc, ed[i]);
-            }
-            catch(IOException ex){
-
-                System.out.println(ex.getMessage());
-            }
-            new Output().rewriteDecryptFileZip(pathToFile, ed33);
-             */
         }
-
-        /**
-        if (encrypt) {
-            Input inputOutput = new Input();
-            inputOutput.openFile(pathToFile);
-            String[] arrStr = inputOutput.readForEncryptFile(pathToFile);
-            inputOutput.closeFile();
-            byte[][] ed = new byte[arrStr.length][];
-            for (int i = 0; i < arrStr.length; i++) {
-                HC128Engine hc_enc = new HC128Engine(iv_srt.getBytes(), key_srt.getBytes());
-                ed[i] = encrypt(hc_enc, arrStr[i].getBytes());
-
-            }
-            new Output().rewriteEncryptFile(pathToFile, ed);
-
-        } else {
-            Input inputOutput = new Input();
-            inputOutput.openFile(pathToFile);
-            byte[][] inputed = inputOutput.readForDecryptFile(pathToFile);
-            inputOutput.closeFile();
-            byte[][] outputed33 = new byte[inputed.length][];
-            for(int i = 0; i < inputed.length; i++) {
-                HC128Engine hc_enc = new HC128Engine(iv_srt.getBytes(), key_srt.getBytes());
-                hc_enc.reset();
-
-                outputed33[i] = encrypt(hc_enc, inputed[i]);
-            }
-            new Output().rewriteDecryptFile(pathToFile, outputed33);
-        }
-         */
-
     }
 
     public static byte[] encrypt(HC128Engine hc, byte[] data) {
